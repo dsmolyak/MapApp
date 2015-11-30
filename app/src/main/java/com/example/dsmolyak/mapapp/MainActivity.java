96646dee4218d7bcd4a8125710551d19b972fcd1
dsmolyak <dsmolyak@gmail.com>
2015-11-30 09:58:49 -0500

    Added Image file

--- app/src/main/java/com/example/dsmolyak/mapapp/MainActivity.java
+++ app/src/main/java/com/example/dsmolyak/mapapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Fight me", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 text.setText("Clicked");
 
