9fa5ab1bd00c057c66c44bd5a04e639cef36a93a
dsmolyak <dsmolyak@gmail.com>
2015-12-15 09:23:38 -0500

    Added all the buttons for the C hallway (they turn green when pressed)

--- app/src/main/java/com/example/dsmolyak/mapapp/MainActivity.java
+++ app/src/main/java/com/example/dsmolyak/mapapp/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.dsmolyak.mapapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,9 +9,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.*;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
     ImageView image;
 
     @Override
@@ -23,6 +23,50 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         image = (ImageView) findViewById(R.id.imageView1);
+
+
+        final ArrayList<Button> classrooms = new ArrayList<>();
+        classrooms.add((Button)findViewById(R.id.C128));
+        classrooms.add((Button)findViewById(R.id.C129));
+        classrooms.add((Button)findViewById(R.id.C130));
+        classrooms.add((Button)findViewById(R.id.C131));
+        classrooms.add((Button)findViewById(R.id.C132));
+        classrooms.add((Button)findViewById(R.id.C133));
+        classrooms.add((Button)findViewById(R.id.C134));
+        classrooms.add((Button)findViewById(R.id.C135));
+        classrooms.add((Button)findViewById(R.id.C136));
+        classrooms.add((Button)findViewById(R.id.C137));
+        classrooms.add((Button)findViewById(R.id.C138));
+        classrooms.add((Button)findViewById(R.id.C139));
+        classrooms.add((Button)findViewById(R.id.C140));
+        classrooms.add((Button)findViewById(R.id.C141));
+        classrooms.add((Button)findViewById(R.id.C142));
+        classrooms.add((Button)findViewById(R.id.C143));
+        classrooms.add((Button)findViewById(R.id.C144));
+        classrooms.add((Button)findViewById(R.id.C145));
+        classrooms.add((Button)findViewById(R.id.C146));
+        classrooms.add((Button)findViewById(R.id.C147));
+        classrooms.add((Button)findViewById(R.id.C148));
+        classrooms.add((Button)findViewById(R.id.C149));
+        classrooms.add((Button)findViewById(R.id.C150));
+        classrooms.add((Button)findViewById(R.id.C151));
+        classrooms.add((Button)findViewById(R.id.C152));
+        classrooms.add((Button)findViewById(R.id.C153));
+        classrooms.add((Button)findViewById(R.id.C154));
+        classrooms.add((Button)findViewById(R.id.C155));
+        classrooms.add((Button)findViewById(R.id.C156));
+
+//        boolean[] clicked = new boolean[29];
+
+        for (int i = 0; i < 29; i++) {
+            final int j = i;
+            classrooms.get(j).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    classrooms.get(j).setBackgroundColor(Color.GREEN);
+                }
+            });
+        }
 
 
     }
